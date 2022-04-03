f3b48ff57f47ddd169077542457044cede927594
Logkesh <logkeshhemanth@gmail.com>
2022-04-03 19:03:36 +0530

    Merge Sorting Algorithm in Python

--- Python/SortingAlgos/mergeSort.py
+++ Python/SortingAlgos/mergeSort.py
@@ -1,2 +1,48 @@
 def mergeSort(numElements: int, array: list, analyser):
-    pass
+    analyser.startTimer()
+    domergeSort(array,analyser)
+    analyser.endTimer()
+
+# Merge Sorting Algorithm
+def domergeSort(array: list, analyser):
+    size = len(array)
+    if analyser.comparegt(size,1):
+        # Partitioning the array into 2 Parts by the middle value
+        middle = size//2
+        Left = array[:middle]
+        Right = array[middle:]
+
+        # Merge Sortng the First half
+        domergeSort(Left,analyser)
+        # Merge sorting the second half
+        domergeSort(Right,analyser)
+
+        index = 0
+        left = 0
+        right = 0
+
+        # Comparing and Storing the array elements 
+        # in a temperary array in a sorted order
+        while left < len(Left) and right < len(Right):
+            analyser.iterate()
+            if analyser.comparelt(Left[left], Right[right]):
+                array[index] = Left[left]
+                left += 1
+            else:
+                array[index] = Right[right]
+                right += 1
+            index += 1
+
+        # Storing the remaing array elements in first half in that temperary array
+        while analyser.comparelt(left, len(Left)):
+            analyser.iterate()
+            array[index] = Left[left]
+            left += 1
+            index += 1
+
+        # Storing the remaing array elements in second half in that temperary array
+        while analyser.comparelt(right,len(Right)):
+            analyser.iterate()
+            array[index] = Right[right]
+            right += 1
+            index += 1
