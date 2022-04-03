6206f063c22ae93b94697e8367e98125c042cfb6
Logkesh <logkeshhemanth@gmail.com>
2022-04-03 10:50:26 +0530

    Quick Sorting Algorithm in Python

---
+++ Python/SortingAlgos/quickSort.py
@@ -0,0 +1,26 @@
+# Quick Sorting Algorithm
+def quickSort(array: list,start: int,end: int,analyser):
+    
+    if(analyser.comparelt(start,end)):
+        # Partitioning The Array
+        partition_val = partition(array,start,end,analyser)
+        # Quick Sorting the array indexing from start to (partition index value - 1)
+        quickSort(array,start,partition_val-1,analyser)
+        # Quick Sorting the array indexing from (partition index value + 1) to end
+        quickSort(array, partition_val+1, end,analyser)
+
+def partition(array: list,start: int,end: int,analyser):
+    # Setting the pivot point
+    pivot = array[end]
+    i = start-1    
+    # Iterating the loop for Sorting
+    for j in range(start,end):
+        analyser.iterate()
+        if(not analyser.comparegt(array[j],pivot)):
+            i+=1
+            # Swaping the array elements
+            array[i],array[j] = analyser.swap(array[i],array[j])
+    # Swaping the array element and the pivot value
+    array[i+1],array[end] = analyser.swap(array[i+1],array[end])
+    # Returning the partition index value
+    return i+1
