ac81a5f514205ca9b337ea77d03442dad6a669e4
KishoreV17 <kishorevenkataramanindia@gmail.com>
2022-04-04 22:29:50 +0530

    selectionSort

---
+++ Cpp/SortingAlgos/bubbleSort.h
@@ -0,0 +1,23 @@
+#include<vector>
+#include<iostream>
+ using namespace std;
+
+void bubbleSort(int numElements, vector<int>& array, CppAnalyser& analyser)
+{
+    analyser.startTimer();
+    int min;
+    for(int i=0;i<numElements;i++)
+    {
+        min=i;
+        for(int j=i+1;j<numElements;j++)
+        {
+            analyser.iterate();
+            if(analyser.comparelt(array[j],array[min]))
+            {
+                min=j;
+            }
+        }
+        analyser.swap(array[i],array[min]);
+    }
+    analyser.endTimer();
+}
